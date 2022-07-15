3a338b41d2c7a0544c3d013a91669909b7c0b86a
WhizzyDoc <hassanridwan@gmail.com>
2022-07-15 12:54:12 +0100

    added a download button

--- id.js
+++ id.js
@@ -51,3 +51,15 @@ function createID() {
   country1.innerHTML = country.value;
   occupation1.innerHTML = occupation.value;
 }
+
+function downloadimage() {
+   var container = document.getElementById("id");
+   html2canvas(container, { allowTaint: true }).then(function (canvas) {
+      var link = document.createElement("a");
+      document.body.appendChild(link);
+      link.download = "my_ID.jpg";
+      link.href = canvas.toDataURL();
+      link.target = "_blank";
+      link.click();
+   });
+}
